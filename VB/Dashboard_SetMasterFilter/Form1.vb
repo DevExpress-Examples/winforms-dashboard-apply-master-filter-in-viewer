af389fcc6cfe8a591af99f2513f08c68ee44eb96
DevExpressExampleBot <fakeEmail>
2022-06-17 20:32:33 +0400

    Source auto update [skip ci]

--- VB/Dashboard_SetMasterFilter/Form1.vb
+++ VB/Dashboard_SetMasterFilter/Form1.vb
@@ -1,4 +1,4 @@
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Windows.Forms
 Imports System.Collections
@@ -9,57 +9,67 @@ Imports System.Linq
 Imports DevExpress.XtraEditors
 
 Namespace Dashboard_SetMasterFilter
+	Partial Public Class Form1
+		Inherits XtraForm
 
-    Public Partial Class Form1
-        Inherits XtraForm
+		Public Sub New()
+			InitializeComponent()
+			AddHandler dashboardViewer1.ConfigureDataConnection, AddressOf DashboardViewer1_ConfigureDataConnection
+			AddHandler dashboardViewer1.CustomizeDashboardTitle, AddressOf DashboardViewer1_CustomizeDashboardTitle
 
-        Public Sub New()
-            InitializeComponent()
-            AddHandler dashboardViewer1.ConfigureDataConnection, AddressOf DashboardViewer1_ConfigureDataConnection
-            AddHandler dashboardViewer1.CustomizeDashboardTitle, AddressOf DashboardViewer1_CustomizeDashboardTitle
-            dashboardViewer1.LoadDashboard("Dashboard.xml")
-            AddHandler dashboardViewer1.MasterFilterSet, AddressOf DashboardViewer1_MasterFilterSet
-            AddHandler dashboardViewer1.MasterFilterCleared, AddressOf DashboardViewer1_MasterFilterCleared
-        End Sub
+			dashboardViewer1.LoadDashboard("Dashboard.xml")
 
-        Private Sub SetMasterFilterMethod()
-            ' Create a list with values to select in the Grid dashboard item.
-            Dim rowValues1 As List(Of Object) = New List(Of Object)()
-            rowValues1.AddRange({"UK", "Anne Dodsworth"})
-            Dim rowValues2 As List(Of Object) = New List(Of Object)()
-            rowValues2.AddRange({"USA", "Andrew Fuller"})
-            Dim selectedRows As List(Of IList) = New List(Of IList)({rowValues1, rowValues2})
-            ' Create a selection range and specify its minimum and maximum values.
-            Dim minimumValue As Date = New DateTime(2015, 3, 1)
-            Dim maximumValue As Date = New DateTime(2016, 3, 1)
-            Dim selectedRange As RangeFilterSelection = New RangeFilterSelection(minimumValue, maximumValue)
-            ' Select the values in the Grid dashboard item.
-            dashboardViewer1.SetMasterFilter("gridDashboardItem1", selectedRows)
-            ' Select the range in the Range Filter dashboard item.
-            dashboardViewer1.SetRange("rangeFilterDashboardItem1", selectedRange)
-        End Sub
+			AddHandler dashboardViewer1.MasterFilterSet, AddressOf DashboardViewer1_MasterFilterSet
+			AddHandler dashboardViewer1.MasterFilterCleared, AddressOf DashboardViewer1_MasterFilterCleared
+		End Sub
 
-        Private Sub DashboardViewer1_MasterFilterSet(ByVal sender As Object, ByVal e As MasterFilterSetEventArgs)
-            Dim viewer As DashboardViewer = CType(sender, DashboardViewer)
-            ' If the Master Filter includes Anne Dodsworth as Sales Person, disable print and export.
-            If e.DashboardItemName.Contains("grid") Then viewer.AllowPrintDashboard = If(e.SelectedValues.[Select](Function(value) value(1).ToString()).Contains("Anne Dodsworth"), False, True)
-        End Sub
+		Private Sub SetMasterFilterMethod()
+			' Create a list with values to select in the Grid dashboard item.
+			Dim rowValues1 As New List(Of Object)()
+			rowValues1.AddRange( { "UK", "Anne Dodsworth" })
+			Dim rowValues2 As New List(Of Object)()
+			rowValues2.AddRange( { "USA", "Andrew Fuller" })
+			Dim selectedRows As New List(Of IList)( { rowValues1, rowValues2 })
 
-        Private Sub DashboardViewer1_MasterFilterCleared(ByVal sender As Object, ByVal e As MasterFilterClearedEventArgs)
-            MessageBox.Show("Selection cleared in the " & e.DashboardItemName & "Master Filter item.")
-        End Sub
+			' Create a selection range and specify its minimum and maximum values.
+			Dim minimumValue As New Date(2015, 3, 1)
+			Dim maximumValue As New Date(2016, 3, 1)
+			Dim selectedRange As New RangeFilterSelection(minimumValue, maximumValue)
 
-        Private Sub DashboardViewer1_CustomizeDashboardTitle(ByVal sender As Object, ByVal e As CustomizeDashboardTitleEventArgs)
-            Dim viewer As DashboardViewer = CType(sender, DashboardViewer)
-            ' Create the command button to set Master Filter.
-            Dim setMasterFilterItem As DashboardToolbarItem = New DashboardToolbarItem("Set Master Filter", New Action(Of DashboardToolbarItemClickEventArgs)(Sub(args) SetMasterFilterMethod()))
-            setMasterFilterItem.Caption = "Set Master Filter"
-            e.Items.Insert(0, setMasterFilterItem)
-        End Sub
+			' Select the values in the Grid dashboard item.
+			dashboardViewer1.SetMasterFilter("gridDashboardItem1", selectedRows)
+			' Select the range in the Range Filter dashboard item.
+			dashboardViewer1.SetRange("rangeFilterDashboardItem1", selectedRange)
+		End Sub
 
-        Private Sub DashboardViewer1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DashboardConfigureDataConnectionEventArgs)
-            Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
-            If parameters IsNot Nothing Then parameters.FileName = Path.GetFileName(parameters.FileName)
-        End Sub
-    End Class
+		Private Sub DashboardViewer1_MasterFilterSet(ByVal sender As Object, ByVal e As MasterFilterSetEventArgs)
+			Dim viewer As DashboardViewer = DirectCast(sender, DashboardViewer)
+			' If the Master Filter includes Anne Dodsworth as Sales Person, disable print and export.
+			If e.DashboardItemName.Contains("grid") Then
+				viewer.AllowPrintDashboard = If(e.SelectedValues.Select(Function(value) value(1).ToString()).Contains("Anne Dodsworth"), False, True)
+			End If
+		End Sub
+
+		Private Sub DashboardViewer1_MasterFilterCleared(ByVal sender As Object, ByVal e As MasterFilterClearedEventArgs)
+			MessageBox.Show("Selection cleared in the " & e.DashboardItemName & "Master Filter item.")
+		End Sub
+
+		Private Sub DashboardViewer1_CustomizeDashboardTitle(ByVal sender As Object, ByVal e As CustomizeDashboardTitleEventArgs)
+			Dim viewer As DashboardViewer = DirectCast(sender, DashboardViewer)
+
+			' Create the command button to set Master Filter.
+			Dim setMasterFilterItem As DashboardToolbarItem = New DashboardToolbarItem("Set Master Filter", New Action(Of DashboardToolbarItemClickEventArgs)(Sub(args)
+				SetMasterFilterMethod()
+			End Sub))
+			setMasterFilterItem.Caption = "Set Master Filter"
+			e.Items.Insert(0, setMasterFilterItem)
+		End Sub
+
+		Private Sub DashboardViewer1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DashboardConfigureDataConnectionEventArgs)
+			Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
+			If parameters IsNot Nothing Then
+				parameters.FileName = Path.GetFileName(parameters.FileName)
+			End If
+		End Sub
+	End Class
 End Namespace
